887056af63616c75f885a70efdbfd1def3942ae3
JULIUS AALTO <72438387+0xjulius@users.noreply.github.com>
2025-01-31 00:50:01 +0200

    Initial commit

---
+++ atm.py
@@ -0,0 +1,53 @@
+def atm_simulator():
+    saldo = 1000.0
+    
+    while True:
+        print("\nTervetuloa Pankkiautomaattiin!")
+        print("1. Saldo")
+        print("2. Talletus")
+        print("3. Otto")
+        print("4. Lopetus")
+        
+        valinta = input("Valitse (1-4): ")
+        
+        if valinta == '1':
+            # tarkistetaan saldo
+            print(f"Saldosi on: {saldo:.2f} €")
+        
+        elif valinta == '2':
+            # talletus
+            try:
+                summa = float(input("Syötä tallettamasi summa: "))
+                if summa > 0:
+                    saldo += summa
+                    print(f"{summa:.2f} € Talletus onnistui!")
+                else:
+                    print("Syötä positiivinen numero")
+            except ValueError:
+                print("Virhe. Syötä numeerinen arvo.")
+
+        elif valinta == '3':
+            # varojen otto
+            try:
+                summa = float(input("Kuinka paljon haluat nostaa? "))
+                if summa > 0:
+                    if saldo >= summa:
+                        saldo -= summa
+                        print(f"{summa:.2f} euron nosto onnistui.")
+                    else:
+                        print("Nosto epäonnistui. Varoja liian vähän.")
+                else:
+                    print("Syötä positiivinen numero.")
+            except ValueError:
+                print("Virhe. Syötä numeerinen arvo.")
+
+        elif valinta == '4':
+            # Exit
+            print("Kiitos, että käytit pankkiautomaattia. Näkemiin!")
+            break
+        
+        else:
+            print("Virheellinen valinta. Yritä uudelleen!")
+
+if __name__ == "__main__":
+    atm_simulator()
